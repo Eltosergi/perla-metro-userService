cbb3a4bfdf7f8e4ff3bad4962919df5451102502
Eltosergi <Sergio.parada@alumnos.ucn.cl>
2025-09-28 22:46:28 -0300

    Feat: Actualizado publicacion para render

--- obj/Release/net9.0/perla-metro-user.AssemblyInfo.cs
+++ obj/Release/net9.0/perla-metro-user.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("perla-metro-user")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3a52f115e27b857910a60c12e562c2f3d6ba1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a3f567f444954bf3db2768a856d5d85d84edef")]
 [assembly: System.Reflection.AssemblyProductAttribute("perla-metro-user")]
 [assembly: System.Reflection.AssemblyTitleAttribute("perla-metro-user")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
